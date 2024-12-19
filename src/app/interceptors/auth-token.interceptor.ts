3fd5739414b0555d1314762c94bb4f536e6e6b5c
Lucas da Luz <daluzdev@gmail.com>
2024-12-19 18:29:01 -0300

    feat(interceptor): - redirect to login when unauthorized logic was implemented

--- src/app/interceptors/auth-token.interceptor.ts
+++ src/app/interceptors/auth-token.interceptor.ts
@@ -1,15 +1,35 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('auth_token');
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  const token = authService.getToken();
   if (!!token) {
-    const clonedRequest = req.clone({
+    req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
     });
-    return next(clonedRequest);
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
+
+        console.warn(
+          'Acesso não autorizado. Redirecionando para a tela de login.'
+        );
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
