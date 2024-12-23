ed5cd4268c7a2be29a3a6faf795fe2aa7fc285d1
Lucas da Luz <daluzdev@gmail.com>
2024-12-23 15:14:56 -0300

    update ngrok api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     //apiUrl: 'https://localhost:44360/api' // URL do backend para desenvolvimento
-    apiUrl: 'https://dfff-177-55-225-203.ngrok-free.app/api'
+    apiUrl: 'https://e12c-177-55-225-203.ngrok-free.app/api'
   };
   
