03e63057599cb2fd140f8734626518843fe3580f
Lucas da Luz <daluzdev@gmail.com>
2024-12-24 10:41:19 -0300

    feat(config): update api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     //apiUrl: 'https://localhost:44360/api' // URL do backend para desenvolvimento
-    apiUrl: 'https://e12c-177-55-225-203.ngrok-free.app/api'
+    apiUrl: 'https://6f60-179-0-73-35.ngrok-free.app/api'
   };
   
