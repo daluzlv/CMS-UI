3bb03073937430cfecebc19259c0f93dbd6f0c72
Lucas da Luz <daluzdev@gmail.com>
2024-12-26 19:26:19 -0300

    fix: remove debuggers

--- src/app/components/auth/confirm-email/confirm-email.component.ts
+++ src/app/components/auth/confirm-email/confirm-email.component.ts
@@ -25,7 +25,6 @@ export class ConfirmEmailComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    debugger;
     this.activatedRoute.queryParams.subscribe((params) => {
       this.userId = params['userId'];
       this.token = params['token'];
